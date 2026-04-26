8611511a34c22bda56018039bf44e315718c8eca
Pablo Sabater <pabloosabaterr@gmail.com>
2026-04-26 17:28:50 +0200

    ir: add emiters for binaries and unaries
    add emitters for binaries and castings. also emit three unaries
    that use bin operations with zero.
    
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- tests/t0006-ir.sh
+++ tests/t0006-ir.sh
@@ -22,4 +22,62 @@ test_expect_success 'function returning constant' '
 	test_cmp expect actual
 '
 
+test_expect_success 'binary operations' '
+	cat >input.orn <<-\EOF &&
+	fn arith() -> int {
+	ret (10 - 3) * 2 + 1;
+	}
+
+	fn cmp() -> bool {
+		ret 5 > 3;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn arith() -> i32 {
+	entry:
+	    %0 = const i32 10
+	    %1 = const i32 3
+	    %2 = sub i32 %0, %1
+	    %3 = const i32 2
+	    %4 = mul i32 %2, %3
+	    %5 = const i32 1
+	    %6 = add i32 %4, %5
+	    ret i32 %6
+	}
+
+	fn cmp() -> i1 {
+	entry:
+	    %0 = const i32 5
+	    %1 = const i32 3
+	    %2 = gt i1 %0, %1
+	    ret i1 %2
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'unary operation' '
+	cat >input.orn <<-\EOF &&
+	fn neg() -> int {
+		ret -5;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn neg() -> i32 {
+	entry:
+	    %0 = const i32 5
+	    %1 = const i32 0
+	    %2 = sub i32 %1, %0
+	    ret i32 %2
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
 test_done
