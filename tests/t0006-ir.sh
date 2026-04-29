f1f388850c29ebde628d2a8777646dd2dbf923f4
Pablo Sabater <pabloosabaterr@gmail.com>
2026-04-29 22:04:36 +0200

    ir: add ir lowering for enums, structs and arrays
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- tests/t0006-ir.sh
+++ tests/t0006-ir.sh
@@ -315,4 +315,162 @@ test_expect_success 'function call and function params' '
 	test_cmp expect actual
 '
 
+test_expect_success 'array init' '
+	cat >input.orn <<-\EOF &&
+	fn main() -> int {
+		let arr: [int; 3] = [1, 2, 3];
+		ret arr[1];
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn main() -> i32 {
+	entry:
+	    %0 = alloc arr
+	    %1 = const i32 0
+	    %2 = gep i32 %0, %1
+	    %3 = const i32 1
+	    store i32 %3, %2
+	    %4 = const i32 1
+	    %5 = gep i32 %0, %4
+	    %6 = const i32 2
+	    store i32 %6, %5
+	    %7 = const i32 2
+	    %8 = gep i32 %0, %7
+	    %9 = const i32 3
+	    store i32 %9, %8
+	    %10 = const i32 1
+	    %11 = gep i32 %0, %10
+	    %12 = load i32 %11
+	    ret i32 %12
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'struct init and member access' '
+	cat >input.orn <<-\EOF &&
+	struct Point {
+		x: int;
+		y: double;
+	}
+
+	fn main() -> double {
+		let obj: Point = Point { x: 1, y: 2.0 };
+		ret obj.y;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn main() -> f64 {
+	entry:
+	    %0 = alloc struct
+	    %1 = const i32 0
+	    %2 = gep i32 %0, %1
+	    %3 = const i32 1
+	    store i32 %3, %2
+	    %4 = const i32 1
+	    %5 = gep f64 %0, %4
+	    %6 = const f64 2.000000
+	    store f64 %6, %5
+	    %7 = const i32 1
+	    %8 = gep f64 %0, %7
+	    %9 = load f64 %8
+	    ret f64 %9
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'enum members' '
+	cat >input.orn <<-\EOF &&
+	enum test {
+		A = 1,
+		B(int),
+		C,
+	}
+
+	fn foo() {
+		let a: test = test::A;
+		let b: test = test::B(42);
+		let c: test = test::C;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn foo() -> void {
+	entry:
+	    %0 = alloc struct
+	    %1 = const i32 1
+	    %2 = const i32 0
+	    %3 = gep i32 %0, %2
+	    store i32 %1, %3
+	    %4 = alloc struct
+	    %5 = const i32 2
+	    %6 = const i32 0
+	    %7 = gep i32 %4, %6
+	    store i32 %5, %7
+	    %8 = const i32 1
+	    %9 = gep arr %4, %8
+	    %10 = const i32 42
+	    %11 = const i32 0
+	    %12 = gep i32 %9, %11
+	    store i32 %10, %12
+	    %13 = alloc struct
+	    %14 = const i32 3
+	    %15 = const i32 0
+	    %16 = gep i32 %13, %15
+	    store i32 %14, %16
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'enum without tagged union' '
+	cat >input.orn <<-\EOF &&
+	enum test { A, B }
+	fn foo() {
+		let a: test = ::A;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn foo() -> void {
+	entry:
+	    %0 = alloc i32
+	    %1 = const i32 0
+	    store i32 %1, %0
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
+test_expect_success 'enum member as expression' '
+	cat >input.orn <<-\EOF &&
+	enum test { A, B }
+	fn foo()-> test {
+		ret ::B;
+	}
+	EOF
+	cat >expect <<-\EOF &&
+	fn foo() -> i32 {
+	entry:
+	    %0 = const i32 1
+	    ret i32 %0
+	}
+
+	Program compiled with 0 errors
+	EOF
+	"$ORN" --dump-ir input.orn >actual &&
+	test_cmp expect actual
+'
+
 test_done
