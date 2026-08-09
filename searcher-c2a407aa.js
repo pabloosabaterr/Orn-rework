561c280d67c03953f375a7da16bb9456e2954244
pabloosabaterr <pabloosabaterr@users.noreply.github.com>
2026-08-09 15:20:41 +0000

    deploy: debe018bca5957f73f879e3051afe6a898004301

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-440894f6.js',
+                path_to_root + 'searchindex-8b47e9e8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
